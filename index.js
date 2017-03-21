4d14d75d2045d3d5bbfa3d36b43556bf13303a97
meghs.t@gmail.com <meghanathiyyakat@Meghanas-MacBook-Air.local>
2017-03-21 22:08:17 +0530

    insert code system into mongodb

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
 const express = require('express')
 var path = require('path');
-const passport = require('passport')  
-const session = require('express-session') 
+const passport = require('passport')
+const session = require('express-session')
 const app = express()
 fileUpload = require('express-fileupload');
 const uuidV1 = require('uuid/v1');
+var MongoClient = require('mongodb').MongoClient , format = require('util').format;
 
 app.use(fileUpload());
 app.use(express.static('public'))
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.sendfile( path.join( __dirname, 'public','login', 'index.html' ));
+app.get('/', function(req, res) {
+		res.sendfile(path.join(__dirname, 'public', 'login', 'index.html'));
 })
-app.get('/home', function (req, res) {
-  res.sendfile( path.join( __dirname, 'public','login', 'index.html' ));
+app.get('/home', function(req, res) {
+		res.sendfile(path.join(__dirname, 'public', 'login', 'index.html'));
+})
+app.get("/upload", function(req, res) {
+		res.sendfile(path.join(__dirname, 'public', 'codesystem', 'fileupload.html'));
+
 })
 app.post('/upload', function(req, res) {
-  if (!req.files)
-    return res.status(400).send('No files were uploaded.');
- 
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file 
- sampleFile = req.files.sampleFile;
- console.log(req.files.sampleFile.name)
- 
-  // Use the mv() method to place the file somewhere on your server 
-  sampleFile.mv('data/'+uuidV1()+'.txt', function(err) {
-    if (err)
-      return res.status(500).send(err);
- 
-    res.send('File uploaded!');
-  });
+	if (!req.files)
+		return res.status(400).send('No files were uploaded.');
+	sampleFile = req.files.sampleFile;
+	codeSystem = JSON.parse(req.files.sampleFile.data)
+	codeSystemOID = uuidV1()
+	codeSystem["codeSystem"] = codeSystemOID
+
+	MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+	  	if (err) throw err;
+	  		console.log("Connected to Database");
+	  
+		//insert record
+		db.collection('codesystem').insert(codeSystem, function(err, records) {
+			if (err) throw err;
+			console.log("Record added");
+			res.send('File uploaded! <br/><br/>Code System Code : ' + codeSystemOID);
+		});
+	});
+
+	
 });
+
 
 
 app.listen(3000);
