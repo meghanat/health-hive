357f1ab0c0026504d01f359ad8bfccb45f46687d
meghanat <meghs.t@gmail.com>
2017-04-18 19:53:21 +0530

    Remove header line from CSV file before importing to hive

--- java/HiveClient.java
+++ java/HiveClient.java
@@ -50,26 +50,32 @@ public class HiveClient {
 	}
 
 	//remove CSV header
-	public static void removeFirstLine(String fileName) throws IOException {  
+	public static void removeFirstLine(String fileName) {
 
-		RandomAccessFile raf = new RandomAccessFile(fileName, "rw");          
-		 //Initial write position                                             
-		long writePosition = raf.getFilePointer();                            
-		raf.readLine();                                                       
-		// Shift the next lines upwards.                                      
-		long readPosition = raf.getFilePointer();                             
+		System.out.println("csv file:"+fileName);  
+		try{
+			RandomAccessFile raf = new RandomAccessFile(fileName, "rw");          
+			 //Initial write position                                             
+			long writePosition = raf.getFilePointer();                            
+			raf.readLine();                                                       
+			// Shift the next lines upwards.                                      
+			long readPosition = raf.getFilePointer();                             
 
-		byte[] buff = new byte[1024];                                         
-		int n;                                                                
-		while (-1 != (n = raf.read(buff))) {                                  
-			raf.seek(writePosition);                                          
-			raf.write(buff, 0, n);                                            
-			readPosition += n;                                                
-			writePosition += n;                                               
-			raf.seek(readPosition);                                           
-		}                                                                     
-		raf.setLength(writePosition);                                         
-		raf.close();                                                          
+			byte[] buff = new byte[1024];                                         
+			int n;                                                                
+			while (-1 != (n = raf.read(buff))) {                                  
+				raf.seek(writePosition);                                          
+				raf.write(buff, 0, n);                                            
+				readPosition += n;                                                
+				writePosition += n;                                               
+				raf.seek(readPosition);                                           
+			}                                                                     
+			raf.setLength(writePosition);                                         
+			raf.close();  
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}                                                        
 	} 
 
 	//create table and load data into table
@@ -171,6 +177,8 @@ public class HiveClient {
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			String header=bufferedReader.readLine();//get header - line 1
 			String createQuery=getCreateQuery(tableName,header);
+			String path=folderName+"/"+fileName;
+			removeFirstLine(path);
 			createTable(createQuery,folderName,fileName,tableName,databaseName);
 			fileReader.close();	
 		}
